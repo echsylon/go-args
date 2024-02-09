2d01beba4ba36cbd56f6dc696b2863b042317dae
László Urszuly <laszlo@echsylon.com>
2024-02-09 17:26:35 +0100

    Add typed value getter alternatives

--- test/args_test.go
+++ test/args_test.go
@@ -27,11 +27,62 @@ func Test_WhenRegisteringInvalidArgument_ThenPanic(t *testing.T) {
 	args.DefineConstrainedArgument("ARG", "Description", "", 0, 1)
 }
 
-func Test_WhenGettingValueForNonRegisteredOption_ThenEmptyStringIsReturned(t *testing.T) {
-	expected := ""
-	actual := args.GetOptionValue("unregisterd")
+func Test_WhenGettingStringValueForNonRegisteredOption_ThenFallbackIsReturned(t *testing.T) {
+	expected := "fallback"
+	actual := args.GetOptionStringValue("unregisterd", expected)
 	if actual != expected {
 		t.Errorf("Expected <%s>, but got <%s>", expected, actual)
+	}
+}
+
+func Test_WhenGettingIntValueForNonIntOption_ThenFallbackIsReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	expected := int64(12)
+	os.Args = []string{"appName", "--value", "string"}
+
+	args.Reset()
+	args.DefineOption("value", "description")
+	args.Parse()
+	actual := args.GetOptionIntValue("value", expected)
+
+	if actual != expected {
+		t.Errorf("Expected <%d>, but got <%d>", expected, actual)
+	}
+}
+
+func Test_WhenGettingFloatValueForNonFloatOption_ThenFallbackIsReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	expected := float64(11.9)
+	os.Args = []string{"appName", "--value", "true"}
+
+	args.Reset()
+	args.DefineOption("value", "description")
+	args.Parse()
+	actual := args.GetOptionFloatValue("value", expected)
+
+	if actual != expected {
+		t.Errorf("Expected <%f>, but got <%f>", expected, actual)
+	}
+}
+
+func Test_WhenGettingBoolValueForNonBoolOption_ThenFallbackIsReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	expected := true
+	os.Args = []string{"appName", "--value", "13.1"}
+
+	args.Reset()
+	args.DefineOption("value", "description")
+	args.Parse()
+	actual := args.GetOptionBoolValue("value", expected)
+
+	if actual != expected {
+		t.Errorf("Expected <%t>, but got <%t>", expected, actual)
 	}
 }
 
@@ -43,20 +94,71 @@ func Test_WhenGettingValueForNonRegisteredArgument_ThenEmptyResultSetIsReturned(
 	}
 }
 
+func Test_WhenGettingIntValuesForPartiallyIntArguments_ThenOnlyIntValuesAreReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	e := []int64{12, 14}
+	os.Args = []string{"appName", "12", "13.1", "14"}
+
+	args.Reset()
+	args.DefineConstrainedArgument("arg", "description", "", 1, 3)
+	args.Parse()
+	a := args.GetArgumentIntValues("arg")
+
+	if len(a) != len(e) || a[0] != e[0] || a[1] != e[1] {
+		t.Errorf("Expected <%v>, but got <%v>", e, a)
+	}
+}
+
+func Test_WhenGettingFloatValuesForPartiallyFloatArguments_ThenOnlyFloatValuesAreReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	e := []float64{3.1, 1.4}
+	os.Args = []string{"appName", "true", "3.1", "1.4"}
+
+	args.Reset()
+	args.DefineConstrainedArgument("arg", "description", "", 1, 3)
+	args.Parse()
+	a := args.GetArgumentFloatValues("arg")
+
+	if len(a) != len(e) || a[0] != e[0] || a[1] != e[1] {
+		t.Errorf("Expected <%v>, but got <%v>", e, a)
+	}
+}
+
+func Test_WhenGettingBoolValuesForPartiallyBoolArguments_ThenOnlyBoolValuesAreReturned(t *testing.T) {
+	actualArgs := os.Args
+	defer func() { os.Args = actualArgs }()
+
+	e := []bool{true, true}
+	os.Args = []string{"appName", "True", "1", "1.4"}
+
+	args.Reset()
+	args.DefineConstrainedArgument("arg", "description", "", 1, 3)
+	args.Parse()
+	a := args.GetArgumentBoolValues("arg")
+
+	if len(a) != len(e) || a[0] != e[0] || a[1] != e[1] {
+		t.Errorf("Expected <%v>, but got <%v>", e, a)
+	}
+}
+
 func Test_WhenParsingRegisteredOptionWithNoValue_ThenBooleanTrueCanBeRetrievedForIt(t *testing.T) {
 	actualArgs := os.Args
 	defer func() { os.Args = actualArgs }()
 
-	expected := "true"
+	expected := true
 	os.Args = []string{"appName", "--value"}
 
 	args.Reset()
 	args.DefineOption("value", "description")
 	args.Parse()
-	actual := args.GetOptionValue("value")
+	actual := args.GetOptionBoolValue("value", false)
 
 	if actual != expected {
-		t.Errorf("Expected <%s>, but got <%s>", expected, actual)
+		t.Errorf("Expected <%t>, but got <%t>", expected, actual)
 	}
 }
 
@@ -70,7 +172,7 @@ func Test_WhenParsingRegisteredOptionWithValue_ThenTheValueCanBeRetrievedUndisto
 	args.Reset()
 	args.DefineOption("v", "description")
 	args.Parse()
-	actual := args.GetOptionValue("v")
+	actual := args.GetOptionStringValue("v", "fallback")
 
 	if actual != expected {
 		t.Errorf("Expected <%s>, but got <%s>", expected, actual)
